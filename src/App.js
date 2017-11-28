fa813a4ee8657241ad246a6123e7c1734b3f4840
IsNology <glenn@isnology.com>
2017-11-28 12:11:58 +1100

    changed to use an array of items

--- src/App.js
+++ src/App.js
@@ -3,10 +3,26 @@ import logo from './logo.svg';
 import './App.css';
 import TodoItem from './components/TodoItem'
 
+const items = [
+  { description: 'First', completed: true },
+  { description: 'Second', completed: false },
+  { description: 'Third', completed: true },
+]
+
 class App extends Component {
   render() {
     return (
       <div className="App">
+        {
+          items.map((item, index) => (
+            <TodoItem
+                key={ index }
+                description={ item.description }
+                completed={ item.completed }
+            />
+          ))
+        }
+        <br />
         <TodoItem description='First' completed />
         <TodoItem description='Second' completed />
         <TodoItem description='Third'/>
